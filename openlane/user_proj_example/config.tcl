1fa8f2170532ea2bdbfbf926a57a3138ae6b0be1
abdullahyildiz <abdullahyildiz@github.com>
2022-06-08 16:23:26 +0300

    Update user_proj_example OpenLane flow script

--- openlane/user_proj_example/config.tcl
+++ openlane/user_proj_example/config.tcl
@@ -22,21 +22,27 @@ set ::env(DESIGN_NAME) user_proj_example
 
 set ::env(VERILOG_FILES) "\
 	$::env(CARAVEL_ROOT)/verilog/rtl/defines.v \
+	$script_dir/../../verilog/rtl/yonga_can_top.v \
+	$script_dir/../../verilog/rtl/yonga_can_controller.v \
+	$script_dir/../../verilog/rtl/yonga_can_packetizer.v \
+	$script_dir/../../verilog/rtl/yonga_can_pulse_gen.v \
 	$script_dir/../../verilog/rtl/user_proj_example.v"
 
 set ::env(DESIGN_IS_CORE) 0
 
 set ::env(CLOCK_PORT) "wb_clk_i"
-set ::env(CLOCK_NET) "counter.clk"
+# set ::env(CLOCK_NET) "counter.clk"
 set ::env(CLOCK_PERIOD) "10"
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 900 600"
+set ::env(DIE_AREA) "0 0 2000 2000"
 
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
-set ::env(PL_BASIC_PLACEMENT) 1
-set ::env(PL_TARGET_DENSITY) 0.05
+set ::env(PL_TIME_DRIVEN) 1
+
+# set ::env(PL_BASIC_PLACEMENT) 1
+# set ::env(PL_TARGET_DENSITY) 0.05
 
 # Maximum layer used for routing is metal 4.
 # This is because this macro will be inserted in a top level (user_project_wrapper) 
